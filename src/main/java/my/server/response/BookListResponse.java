c7516cf10c3352313e9105ba43d4ce94125dd365
Paul Molchanov <pmolch3.1415@gmail.com>
2023-02-08 20:49:00 +0300

    Change items in book list response file

--- src/main/java/my/server/response/BookListResponse.java
+++ src/main/java/my/server/response/BookListResponse.java
@@ -1,27 +1,16 @@
 package my.server.response;
 
+import lombok.RequiredArgsConstructor;
 import my.server.entity.BookEntity;
 import lombok.Data;
 
-import java.util.ArrayList;
-
 @Data
+@RequiredArgsConstructor
 public class BookListResponse extends BaseResponse {
-    private Iterable<BookEntity> books = new ArrayList<>();
-    private BookEntity book;
+    private Iterable<BookEntity> data;
 
-    public BookListResponse(boolean success, String message, BookEntity book) {
-        super(success, message);
-        this.book = book;
-    }
-
-    public BookListResponse(BookEntity book) {
-        super(true, "Book data");
-        this.book = book;
-    }
-
-    public BookListResponse(Iterable<BookEntity> bookEntities) {
-        super(true, "Books data");
-        this.books = bookEntities;
+    public BookListResponse(Iterable<BookEntity> data) {
+        super(true, "Список книг");
+        this.data = data;
     }
 }
