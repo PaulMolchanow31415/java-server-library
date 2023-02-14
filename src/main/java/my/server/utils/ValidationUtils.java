50a21950897c6d295542ac75b25909f52c95b345
PaulMolchanov31415 <pmolch3.1415@gmail.com>
2023-02-14 13:05:13 +0300

    refactor validation utils

--- src/main/java/my/server/utils/ValidationUtils.java
+++ src/main/java/my/server/utils/ValidationUtils.java
@@ -6,7 +6,6 @@ import my.server.exception.ValidationException;
 public class ValidationUtils {
     public static void validateBook(BookEntity book) throws ValidationException {
         StringBuilder errorsMessage = new StringBuilder();
-        boolean hasError = false;
         String title = book.getTitle();
         String author = book.getAuthor();
         String publisher = book.getPublisher();
@@ -15,30 +14,20 @@ public class ValidationUtils {
 
         if (title == null || title.isBlank() || title.length() < 3 || title.length() > 255) {
             errorsMessage.append("Не правильно введено заглавие").append("|");
-            hasError = true;
         }
-
         if (author == null || author.isBlank() || !author.matches("\\w{2,64}\\s\\w{2,64}")) {
             errorsMessage.append("Не правильно введено название автора").append("|");
-            hasError = true;
         }
-
         if (publisher == null || publisher.isBlank() || publisher.length() < 3 || publisher.length() > 255) {
             errorsMessage.append("Не правильно введен издатель").append("|");
-            hasError = true;
         }
-
         if (kind == null || kind.isBlank() || kind.length() < 3 || kind.length() > 255) {
             errorsMessage.append("Не правильно введен жанр книги").append("|");
-            hasError = true;
         }
-
         if (year == null || year.isBlank() || !year.matches("^\\d{4}$")) {
             errorsMessage.append("Не правильно введен год издания").append("|");
-            hasError = true;
         }
-
-        if (hasError) {
+        if (errorsMessage.length() > 0) {
             throw new ValidationException(errorsMessage.toString());
         }
     }
